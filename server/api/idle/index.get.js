3baf35947740776825e7b0f5362171b7150abdb7
xzgun89425 <68765411+xzgun89425@users.noreply.github.com>
2023-08-14 02:34:43 +0800

    clear api

--- server/api/idle/index.get.js
+++
@@ -1,6 +0,0 @@
-// import idleModel from '@/server/model/idle.model';
-
-// export default defineEventHandler(async (event) => {
-//     const idle = await idleModel.find({});
-//     return idle;
-// });
--- server/api/member/index.get.js
+++
@@ -1,6 +0,0 @@
-// import memberModel from '@/server/model/member.model';
-
-// export default defineEventHandler(async (event) => {
-//     const member = await memberModel.find({});
-//     return member;
-// });
